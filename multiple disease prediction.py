0e7c687958b542b39ab7b258e9a195b8c222ba41
y25246001-MScAIBDA <y25246001@dhsgsu.edu.in>
2026-04-11 14:16:08 +0530

    Refactor multiple disease prediction script

--- multiple disease prediction.py
+++ multiple disease prediction.py
@@ -247,8 +247,7 @@ if (selected == "Parkinsons Prediction"):
           parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
     st.success(parkinsons_diagnosis)
-
-    else:
-            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+else:
+    parkinsons_diagnosis = "The person does not have Parkinson's disease"
 
     st.success(parkinsons_diagnosis)
